85111ce83413430a50f73a6877397df662e5aa20
Mateusz Ścianek <scianek@protonmail.com>
2025-01-07 17:33:15 +0100

    feat: return userId when logging in

--- cinemaProject/src/main/java/monaditto/cinemaproject/RESTcontrollers/DTO/AuthResponse.java
+++ cinemaProject/src/main/java/monaditto/cinemaproject/RESTcontrollers/DTO/AuthResponse.java
@@ -5,10 +5,23 @@ import monaditto.cinemaproject.role.RoleDto;
 import java.util.List;
 
 public class AuthResponse {
+    private Long userId;
+
     private List<RoleDto> roles;
 
-    public AuthResponse(List<RoleDto> roles) {
+    public AuthResponse() {}
+
+    public AuthResponse(Long userId, List<RoleDto> roles) {
+        this.userId = userId;
         this.roles = roles;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public List<RoleDto> getRoles() {
